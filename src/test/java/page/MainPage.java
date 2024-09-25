35312406793293a3d49d97991b13f9ffbedf5b77
ZsoltEmber <132936073+ZsoltEmber@users.noreply.github.com>
2024-09-25 13:19:01 +0200

    add MainPage and Post component

---
+++ src/test/java/page/MainPage.java
@@ -0,0 +1,35 @@
+package page;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.FluentWait;
+
+public class MainPage extends BasePage {
+
+
+    @FindBy(className = "createAnswerEmblem")
+    private WebElement createPostButton;
+
+    @FindBy(className = "homeEmblem")
+    private WebElement homeButton;
+
+    @FindBy(className = "logoutEmblem")
+    private WebElement logoutButton;
+
+    @FindBy(id = "dark")
+    private WebElement darkBackground;
+    @FindBy(id = "light")
+    private WebElement lightBackground;
+    @FindBy(xpath = "//button[@class='darkLightBtn']/img[@alt='moon']")
+    private WebElement themeSwitchButton;
+
+    @FindBy(id = "homeEmblem")
+    private WebElement homePageButton;
+    @FindBy(id = "createAnswerEmblem")
+    private WebElement createNewPostButton;
+
+    public MainPage(WebDriver driver, FluentWait<WebDriver> wait) {
+        super(driver, wait);
+    }
+}
